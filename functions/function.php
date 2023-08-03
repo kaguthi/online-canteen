4b994adf8c1ce8fb32f3b4521ca9ddb2592e33b1
Kaguthi <peterkaguathi873@gmail>
2023-08-03 13:00:19 +0300

    otp-correction

--- functions/function.php
+++ functions/function.php
@@ -4,21 +4,19 @@ function search($image, $price, $name){
     $response = '
     <div class="col-md-3 mb-2">
         <div class="card rounded shadow">
+            <img src="admin/foodImage/'.$image.'" alt="food image" class="w-100" height="150px">
             <div class="card-body">
-                <img src="admin/foodImage/'.$image.'" alt="food image" class="w-100" height="150px">
                 <h6>Name: '.$name.'</h6>
                 <p>Price: '.$price.'</p>
-                <button class="btn btn-success center mb-1">Add to cart</button>
+                <i class="bx bxs-star"></i>
+                <i class="bx bxs-star"></i>
+                <i class="bx bxs-star"></i>
+                <i class="bx bxs-star-half"></i>
+                <button class="btn btn-primary center mb-1">Add to cart</button>
             </div>
         </div>
     </div>
     ';
     echo $response;
-}
-
-function getCartItems(){
-    global $con;
-    // $fetch_data = "SELECT c.prod_id as cprod, f.food_id as fid, f.food_image FROM cart c, food f WHERE c.prod_id =f.food_id AND c.user_id = '$userid'";
-
 }
 ?>
